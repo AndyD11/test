f1c80dacad31a19795dc84739646e9a1d74e7ffb
Andy Dong <adong@falcnlab.com>
2020-09-17 16:13:16 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+//components
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//routes
+import Home from './components/home/home'
+import Registration from './components/registration/registration'
+
+//css
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/registration' component={Registration} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
